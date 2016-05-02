f66dd7d015257e2deffb5a135ac9190620aa7b40
Peter Lord <pmlord@gmail.com>
2016-05-02 07:17:42 -0400

    formatting tweak

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -7,11 +7,10 @@ module Helpers
     WebMock.stub_request(:get, url).
       to_return(body: File.new("#{ROOT_PATH}/fixtures/http_stubs/#{url}.txt"))
   end
-  
+
   def stub_timeout(url)
     WebMock.stub_request(:get, url).
       to_timeout
   end
-  
 
 end
